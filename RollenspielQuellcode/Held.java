f53e69c962d8c53fb89acbf2c29c1544b0eafe10
JulianM2009 <julian.mussmann@gmail.com>
2025-12-11 08:28:55 +0100

    xp zu hp und grammatik fehler angepasst

--- RollenspielQuellcode/Held.java
+++ RollenspielQuellcode/Held.java
@@ -8,7 +8,7 @@ public class Held {
     private Drache meinDrache;
 
     public Held(int pLebenspunkte, int pAngriffschaden, String pFaehigkeit, String pName) {
-    
+
     }
 
     public Held() {
@@ -34,18 +34,17 @@ public class Held {
         int schlagen = meineWaffe.schlagen();//Wir lassen uns die Angriffskraft der Waffe geben     
         int angriff = (int) schlagen+erfahrung;            
         int neueErfahrung = zufallszahl(0,8);  
-        System.out.println("Der Drache wurde mit "+angriff+" XP Angegriffen, du hast "+neueErfahrung+" Erfahrungspunkte dazugewonnen");
+        System.out.println("Der Drache wurde mit "+angriff+" HP Angegriffen, du hast "+neueErfahrung+" Erfahrungspunkte dazugewonnen");
         erfahrung += neueErfahrung;
         meinDrache.blocken(angriff); 
-        
     }
 
     public void heilen () {
         if (lebenspunkte < 100) {
-                if(zufallszahl(0,100)>40){
-                    int heilung = zufallszahl(30,50);  
-                    lebenspunkte += heilung;
-                    System.out.println(" Der Held hat sich um "+ heilung +" Lebenspunkte geheilt");
+            if(zufallszahl(0,100)>40){
+                int heilung = zufallszahl(30,50);  
+                lebenspunkte += heilung;
+                System.out.println(" Der Held hat sich um "+ heilung +" Lebenspunkte geheilt");
             }else{
                 System.out.println(" Das Heilen war leider nicht erfolgreich, es muss der falsche Trank gewesen sein.");
             }  
@@ -53,15 +52,16 @@ public class Held {
             System.out.println(" Der Held ist nicht verletzt!");
         }
     }
+
     private int zufallszahl(int min, int max){
         return (int) (Math.random()*(max-min)+min);
     }
-    
+
     public void spiele(int runden){
         for(int i = 0; i< runden;i++){
             angriff();
             blocken();
         }
-        
+
     }
 }
